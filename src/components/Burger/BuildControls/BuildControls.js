37ec3f4b8b61a56424e200f95cd8b478ef2e6dc3
Nick Zahornyi <nick_surveyor@ukr.net>
2018-02-17 17:55:45 +0200

    added build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import classes from "./BuildControls.css";
+
+const controls = [
+  {
+    label: "Salad",
+    type: "salad",
+  },
+  {
+    label: "Bacon",
+    type: "bacon",
+  },
+  {
+    label: "Cheese",
+    type: "cheese",
+  },
+  {
+    label: "Meat",
+    type: "meat",
+  },
+];
+
+const buildControls = props => <div />;
+
+export default buildControls;
